27e8331857562b24f1f39670be0db4b0273b10f5
Artem <artem.uholkov@ukr.net>
2022-09-28 10:01:10 +0300

    language switcher ready

--- src/components/LanguageSwitcher/LanguageSwitcher.js
+++ src/components/LanguageSwitcher/LanguageSwitcher.js
@@ -1,39 +1,37 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './languageSwitcher.scss';
 
 function LanguageSwitcher() {
 
     const [open, setOpen] = useState(false);
+    let [changeFlag, setChange] = useState(true);
 
-    let menuRef = useRef();
+    
 
     useEffect(() => {
         let handler = (e) =>{
-            if(!menuRef.current.contains(e.target)){
             setOpen(false);
-        }
         };
         document.addEventListener("mousedown", handler);
-
         return() => {
             document.removeEventListener("mousedown", handler);
         }
     });
-
+    
     return (
-        <div className="lang_container" ref={menuRef}>
+        <div className="lang_container" >
             <div className="lang_switcher" onClick={() => {setOpen(!open)}} >
-                <img src="./img/languageSwitcher/UA-flag-icon.png" alt="UA Flag" className="flag_icon" />
+                <img src={`./img/languageSwitcher/${changeFlag ? 'UA-flag-icon' : 'RU-flag-icon'}.png`} alt="UA Flag" className="flag_icon" />
                 <img src="./img/languageSwitcher/Vector.png" alt="Arrow switcher" className={`arrow_icon ${open? 'rotate' : 'norotate'}`} />
             </div>
             <div className="wrapper">
                 <div className={`drowdown ${open? 'active' : 'inactive'}`}>
-                    <div className="language" >
-                        <img src="./img/dropdownPopup/UA-flag-icon.png" alt="UA flag" className="flag" />
+                    <div className="language" onClick={() => {setChange(changeFlag = true)}}>
+                        <img src="./img/dropdownPopup/UA-flag-icon.png" alt="UA flag" className="flag"  />
                         <span className="language_text">Українська</span>
                     </div>
-                     <div className="language">
+                     <div className="language" onClick={() => {setChange(changeFlag = false)}}>
                         <img src="./img/dropdownPopup/RU-flag-icon.png" alt="RU flag" className="flag" />
                         <span className="language_text">Раша</span>
                      </div>
