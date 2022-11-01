5ad95515a146d094544011eda36e0d8b40b6dacf
Artem <artem.uholkov@ukr.net>
2022-11-01 13:49:07 +0200

    data created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import BottomHeader from "./components/BottomHeader/BottomHeader";
-import Header from "./components/TopHeader/TopHeader";
+import BottomHeader from './components/BottomHeader/BottomHeader';
+import Header from './components/TopHeader/TopHeader';
+import data from '../src/assets/data.json';
 
+console.log(data);
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <BottomHeader/>
+      <BottomHeader />
     </div>
-    
   );
 }
 
