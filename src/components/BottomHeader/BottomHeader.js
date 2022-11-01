00078914b446fd5db44d5bacaa37b7e9f08e48a3
Artem <artem.uholkov@ukr.net>
2022-11-01 14:25:22 +0200

    started dropdown

--- src/components/BottomHeader/BottomHeader.js
+++ src/components/BottomHeader/BottomHeader.js
@@ -1,21 +1,46 @@
-import React from "react";
-import NavbarButton from "../NavbarButton/NavbarButton";
+import React, { useState, useEffect } from 'react';
+import NavbarButton from '../NavbarButton/NavbarButton';
 
 import './bottomHeader.scss';
 
-function BottomHeader () {
+function BottomHeader() {
+  const [openDropdown, setOpenDropdown] = useState(false);
+  console.log(openDropdown);
 
-    return (
-        <div className="bottom_header_wrapper">
-            <ul className="navbar">
-                <li className="navbar_dropdown"><NavbarButton name={`Товари`}/></li>
-                <li className="navbar_dropdown"><NavbarButton name={`Послуги`}/></li>
-                <a href="#"><li className="navbar_link">Про компанію</li></a>
-                <a href="#"><li className="navbar_link">Контакти</li></a>
-            </ul>
-        </div>
-    )
+  useEffect(() => {
+    let handler = (e) => {
+      setOpenDropdown(false);
+    };
+    document.addEventListener('mousedown', handler);
+    return () => {
+      document.removeEventListener('mousedown', handler);
+    };
+  });
 
+  return (
+    <div className="bottom_header_wrapper">
+      <ul className="navbar">
+        <li className="navbar_dropdown">
+          <NavbarButton
+            name={`Товари`}
+            onClick={() => {
+              setOpenDropdown(!openDropdown);
+            }}
+          />
+        </li>
+        <li className="navbar_dropdown">
+          <NavbarButton name={`Послуги`} />
+        </li>
+        <a href="#">
+          <li className="navbar_link">Про компанію</li>
+        </a>
+        <a href="#">
+          <li className="navbar_link">Контакти</li>
+        </a>
+      </ul>
+      <div className="dropdown_wrapper"></div>
+    </div>
+  );
 }
 
 export default BottomHeader;
